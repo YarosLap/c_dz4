7428718d643f63ac80d1cb6eae20e2065fcc4579
YarosLap <yaroslap86@gmail.com>
2023-02-11 22:05:20 +0300

    first commit

---
+++ task25/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear(); 
+
+Console.WriteLine("Введите число А: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int res = a;
+
+for (int i = 1; i < b; i++)
+{
+    res = res * a;
+}
+
+Console.WriteLine(res);
---
+++ task27/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear(); 
+
+Console.WriteLine("Введите число: ");
+String StringNumber=Console.ReadLine(); 
+
+int sum = 0;         
+Char[]CharNumber = StringNumber.ToCharArray(); 
+for (int i = 0; i < StringNumber.Length; i++)
+{
+    sum += Convert.ToInt32(StringNumber[i]-'0');               
+}
+Console.WriteLine("Сумма цифр в этом числе равна " + sum);
+          
